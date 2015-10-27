4d7752cd7b354fb260c7160b59d34726d89c412d
Adam Bedford <abedford@mavenlink.com>
2015-10-27 14:55:49 -0700

    Add additional fields to Expense model

--- lib/intacct/models/expense.rb
+++ lib/intacct/models/expense.rb
@@ -6,9 +6,19 @@ module Intacct
 
       def create
         send_xml('create') do |xml|
-          xml.function(controlid: "1") {
+          xml.function(controlid: '1') {
             xml.create_expense {
-              expense_xml(xml)
+              create_expense_xml(xml)
+            }
+          }
+        end
+      end
+
+      def update
+        send_xml('update') do |xml|
+          xml.function(controlid: '1') {
+            xml.update_expense(key: key) {
+              update_expense_xml(xml)
             }
           }
         end
@@ -16,7 +26,7 @@ module Intacct
 
       private
 
-      def expense_xml(xml)
+      def create_expense_xml(xml)
         xml.employeeid object.employeeid
         xml.datecreated  {
           xml.year  object.datecreated.try(:strftime, "%Y")
@@ -41,6 +51,73 @@ module Intacct
               xml.memo         expense[:memo]
               xml.locationid   expense[:locationid]
               xml.departmentid expense[:departmentid]
+            }
+          }
+        }
+      end
+
+      def update_expense_xml(xml)
+        xml.employeeid object.employeeid
+        xml.datecreated  {
+          xml.year  object.datecreated.try(:strftime, "%Y")
+          xml.month object.datecreated.try(:strftime, "%m")
+          xml.day   object.datecreated.try(:strftime, "%d")
+        }
+        xml.expensereportno object.expensereportno
+        xml.description     object.description
+        xml.basecurr        object.basecurr
+        xml.currency        object.currency
+
+        if object.customfields
+          xml.customfields {
+            object.customfields.each do |customfield|
+              xml.customfield {
+                xml.customfieldname customfield.name
+                xml.customfieldvalue customfield.value
+              }
+            end
+          }
+        end
+
+        xml.updateexpenses {
+          object.updateexpenses.each { |updateexpense|
+            xml.updateexpense {
+              xml.expensetype  updateexpense[:expensetype]
+              xml.glaccountno  updateexpense[:glaccountno]
+              xml.amount       updateexpense[:amount]
+              xml.currency     updateexpense[:currency]
+              xml.trx_amount   updateexpense[:trx_amount]
+              xml.exchratedate  {
+                xml.year  updateexpense[:exchratedate].try(:strftime, "%Y")
+                xml.month updateexpense[:exchratedate].try(:strftime, "%m")
+                xml.day   updateexpense[:exchratedate].try(:strftime, "%d")
+              }
+              xml.exchratetype  updateexpense[:exchratetype]
+              xml.exchrate      updateexpense[:exchrate]
+              xml.expensedate  {
+                xml.year  updateexpense[:expensedate].try(:strftime, "%Y")
+                xml.month updateexpense[:expensedate].try(:strftime, "%m")
+                xml.day   updateexpense[:expensedate].try(:strftime, "%d")
+              }
+              xml.memo         updateexpense[:memo]
+              xml.locationid   updateexpense[:locationid]
+              xml.departmentid updateexpense[:departmentid]
+              if updateexpense[:customfields]
+                xml.customfields {
+                  updateexpense[:customfields].each do |customfield|
+                    xml.customfield {
+                      xml.customfieldname customfield[:name]
+                      xml.customfieldvalue customfield[:value]
+                    }
+                  end
+                }
+              end
+              xml.projectid   updateexpense[:projectid]
+              xml.customerid  updateexpense[:customerid]
+              xml.vendorid    updateexpense[:vendorid]
+              xml.employeeid  updateexpense[:employeeid]
+              xml.itemid      updateexpense[:itemid]
+              xml.classid     updateexpense[:classid]
             }
           }
         }
