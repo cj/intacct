0554a7c6e26b9faa8790726a06e7265305ffa0a5
Adam Bedford <abedford@mavenlink.com>
2015-11-02 10:57:40 -0800

    Outline of UpdateAll action

--- lib/intacct/actions/update_all.rb
+++ lib/intacct/actions/update_all.rb
@@ -2,18 +2,27 @@ module Intacct
   module Actions
     class UpdateAll < Base
 
-      # def request(options)
-      #   Intacct::XmlRequest.build_xml(client, action) do |xml|
-      #     xml.function(controlid: '1') {
-      #       xml.update {
-      #
-      #       }
-      #     }
-      #   end
-      #
-      # end
+      def request(options)
+        Intacct::XmlRequest.build_xml(client, action) do |xml|
+          xml.function(controlid: '1') {
+            xml.update {
+              #TODO: Finish this
+            }
+          }
+        end
+      end
 
+      def response_body
 
+      end
+
+      def list_type
+
+      end
+
+      def response_errors
+
+      end
 
       module Helper
         include ActiveSupport::Concern
@@ -24,8 +33,9 @@ module Intacct
                 ids:        ids,
                 attributes: attributes
             }
-
             response = Intacct::Actions::UpdateAll.new(client, self, 'update_all', params).perform
+
+            @errors = response.errors
 
             response.success?
           end
