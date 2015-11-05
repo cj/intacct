7291f0cae8f43db2be2e4174d669486eb2d64165
Adam Bedford <abedford@mavenlink.com>
2015-11-05 12:25:26 -0800

    project xml is now in v3.0 format

--- lib/intacct/models/project.rb
+++ lib/intacct/models/project.rb
@@ -6,52 +6,77 @@ module Intacct
         xml.projectid attributes.projectid
         xml.name attributes.name
         xml.description attributes.description
-        xml.parentid attributes.parentid
-        # xml.invoicewithparent attributes.invoicewithparent
+
+        xml.currency attributes.currency
         xml.projectcategory attributes.projectcategory
-        xml.projecttype attributes.projecttype
+        xml.projectstatuskey attributes.projectstatuskey
         xml.projectstatus attributes.projectstatus
+        xml.preventtimesheet attributes.preventtimesheet
+        xml.preventexpense attributes.preventexpense
+        xml.preventappo attributes.preventappo
+        xml.preventgeninvoice attributes.preventgeninvoice
+        # xml.status attributes.status
+
+        xml.begindate attributes.begindate.try(:strftime, '%m/%d/%Y')
+        xml.enddate attributes.enddate.try(:strftime, '%m/%d/%Y')
+
+        xml.budgetamount attributes.budgetamount
+        xml.contractamount attributes.contractamount
+        xml.actualamount attributes.actualamount
+        xml.budgetqty attributes.budgetqty
+
+        xml.percentcomplete attributes.percentcomplete
+        xml.obspercentcomplete attributes.obspercentcomplete
+
+        xml.customerkey attributes.cutomerkey
         xml.customerid attributes.customerid
-        xml.managerid attributes.managerid
-        xml.custuserid attributes.custuserid
-        xml.salescontactid attributes.salescontactid
-        xml.begindate {
-          xml.year attributes.begindate.try(:strftime, "%Y")
-          xml.month attributes.begindate.try(:strftime, "%m")
-          xml.day attributes.begindate.try(:strftime, "%d")
-        }
-        xml.enddate {
-          xml.year attributes.enddate.try(:strftime, "%Y")
-          xml.month attributes.enddate.try(:strftime, "%m")
-          xml.day attributes.enddate.try(:strftime, "%d")
-        }
+
+        xml.projecttypekey attributes.projecttypekey
+        xml.projecttype attributes.projecttype
+
         xml.departmentid attributes.departmentid
         xml.locationid attributes.locationid
-        # xml.classid attributes.classid
-        xml.currency attributes.currency
-        xml.billingtype attributes.billingtype
-        xml.termname attributes.termname
-        xml.docnumber attributes.docnumber
-        xml.sonumber attributes.sonumber
-        xml.ponumber attributes.ponumber
-        xml.poamount attributes.poamount
-        xml.pqnumber attributes.pqnumber
-        # xml.budgetamount attributes.budgetamount
-        # xml.budgetedcost attributes.budgetedcost
-        # xml.budgetqty attributes.budgetqty
-        xml.userrestrictions attributes.userRestrictions
-        # xml.obspercentcomplete attributes.obspercentcomplete
-        # xml.budgetid attributes.budgetid
-        # xml.billingrate attributes.billingrate
-        # xml.billingpricing attributes.billingpricing
-        # xml.expenserate attributes.expenserate
-        # xml.expensepricing attributes.expensepricing
-        # xml.poaprate attributes.poaprate
-        # xml.poappricing attributes.poappricing
-        xml.status attributes.status
-        # xml.supdocid attributes.supdocid
-        # xml.invoicemessage attributes.invoicemessage
-        # xml.invoicecurrency attributes.invoicecurrency
+
+
+
+
+
+        # xml.parentid attributes.parentid
+        # # xml.invoicewithparent attributes.invoicewithparent
+        # xml.projecttype attributes.projecttype
+        # xml.customerid attributes.customerid
+        # xml.managerid attributes.managerid
+        # xml.custuserid attributes.custuserid
+        # xml.salescontactid attributes.salescontactid
+        # xml.departmentid attributes.departmentid
+        # xml.locationid attributes.locationid
+        # # xml.classid attributes.classid
+        # xml.billingtype attributes.billingtype
+        # xml.termname attributes.termname
+        # xml.docnumber attributes.docnumber
+        #
+        # xml.billto attributes.billto
+        # xml.shipto attributes.shipto
+        #
+        # xml.sonumber attributes.sonumber
+        # xml.ponumber attributes.ponumber
+        # xml.poamount attributes.poamount
+        # xml.pqnumber attributes.pqnumber
+        # # xml.budgetedcost attributes.budgetedcost
+        # # xml.budgetqty attributes.budgetqty
+        # xml.userrestrictions attributes.userRestrictions
+        # # xml.obspercentcomplete attributes.obspercentcomplete
+        # # xml.budgetid attributes.budgetid
+        # # xml.billingrate attributes.billingrate
+        # # xml.billingpricing attributes.billingpricing
+        # # xml.expenserate attributes.expenserate
+        # # xml.expensepricing attributes.expensepricing
+        # # xml.poaprate attributes.poaprate
+        # # xml.poappricing attributes.poappricing
+        # xml.status attributes.status
+        # # xml.supdocid attributes.supdocid
+        # # xml.invoicemessage attributes.invoicemessage
+        # # xml.invoicecurrency attributes.invoicecurrency
 
         if attributes.projectresources
           xml.projectresources {
