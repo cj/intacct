c1dfb51e758d8f653a6b498bd33c044216750ea2
arincon9 <arincon@mavenlink.com>
2018-02-02 10:26:27 -0600

    Add `itemid` field to expenses

--- lib/intacct/models/expense.rb
+++ lib/intacct/models/expense.rb
@@ -43,6 +43,7 @@ module Intacct
               xml.departmentid expense[:departmentid]
               xml.projectid    expense[:projectid]
               xml.billable     expense[:billable]
+              xml.itemid       expense[:itemid]
             }
           }
         }
